c7a2b4839e175d0166eccdfd4acf297d30ac5c9f
Novemberto <koukpeloustallone@gmail.com>
2023-07-19 13:46:56 +0100

    first commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- website/views.py
+++ website/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
+from django.core.mail import send_mail, BadHeaderError
+
 # Create your views here.
+from .models import Contact
+
+
+
+def index(request):
+    
+    
+    
+    return render(request, 'base.html',)
